4cbcd74070364de7d1ae4732662199e2b290493e
Mugundh J B <mugundhjb@gmail.com>
2025-01-01 13:44:15 +0530

    Time: 0 ms (100%), Space: 8.9 MB (23.35%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n = s.length() - 1;
-        int ones = 0, zeros = 0, maxCnt = 0;
-        for(int i = 0; i <= n; i++){
-            if(s[i] == '1') ones++;
+        int ones = count(s.begin(), s.end(), '1');
+        
+        int ans = 0;
+        int zeros = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                ones--;
+            } else {
+                zeros++;
+            }
+            
+            ans = max(ans, zeros + ones);
         }
-        for(int i = 0; i < n; i++){
-            if(s[i] != '1') zeros++;
-            else ones--;
-            maxCnt = max(maxCnt, zeros + ones);
-        }
-        return maxCnt;
+        
+        return ans;
     }
 };
